7419be708ee122a3a370ad11a6ce766a18b80979
RevathyRav <revs_eee@yahoo.co.in>
2016-07-14 12:09:07 +0530

    Add files via upload

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,14 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list-rentals');
+
+test('should filter the list of rentals by city.', function (assert) {
+  visit('/');
+  fillIn('.list-filter input', 'seattle');
+  keyEvent('.list-filter input', 'keyup', 69);
+  andThen(function () {
+    assert.equal(find('.listing').length, 1, 'should show 1 listing');
+    assert.equal(find('.listing .location:contains("Seattle")').length, 1, 'should contain 1 listing with location Seattle');
+  });
+});
